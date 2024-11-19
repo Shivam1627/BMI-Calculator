591216d524792347865d42fedab0358888775ce0
Shivam Singh <166328955+Shivam1627@users.noreply.github.com>
2024-11-19 17:01:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("form");    
+    const form = document.querySelector("form");    
     form.addEventListener('submit', function(e){
     e.preventDefault();
     const height =  parseInt(document.querySelector('#height').value);
@@ -8,7 +8,7 @@ const form = document.querySelector("form");
     { 
      result.innerHTML = `Please give a valid ${height}`;
     }
-    if(weight=="" || weight<0 || isNaN(weight))
+    else if(weight=="" || weight<0 || isNaN(weight))
     {
      result.innerHTML = `Please give a valid ${weight}`;
     }
@@ -17,5 +17,4 @@ const form = document.querySelector("form");
         const bmi = (weight/((height*height)/10000)).toFixed(2);
         result.innerHTML=`<span>${bmi}</span>`;
     }
-
 })
